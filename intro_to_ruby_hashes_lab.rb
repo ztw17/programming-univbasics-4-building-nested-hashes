cea905d4959c8d96a9f712b1fea0904c4bb8197a
Zach Weber <weberzt@gmail.com>
2019-12-11 14:41:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,8 +50,10 @@ def monopoly_with_fourth_tier
           "mortgage_value" => "$100"},
         :pennsylvania_railroad => {
           "mortgage_value" => "$200"},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
+        :b_and_o_railroad => {
+          "mortgage_value" => "$400"},
+        :shortline_railroad => {
+          "mortgage_value" => "$800"},
       }
     }
   }
